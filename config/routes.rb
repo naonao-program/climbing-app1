10234039b25d65b71c9ea34aa7be15abda85d47e
naonao-program <gf19a012@oecu.jp>
2021-03-03 21:26:27 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rock_information#index"
 
-  resources :rock_information, only:[:new, :create]
+  resources :rock_information, only:[:index,:new, :create]
 end
