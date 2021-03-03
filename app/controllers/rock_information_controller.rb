0018aafafdc775e7c3e38f2c2df387195937e23c
naonao-program <gf19a012@oecu.jp>
2021-03-03 14:01:54 +0900

    複数画像を投稿できるようにした

--- app/controllers/rock_information_controller.rb
+++ app/controllers/rock_information_controller.rb
@@ -1,6 +1,7 @@
 class RockInformationController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
+    @rock = RockInformation.include(:user)
   end
 
   def new
@@ -19,6 +20,6 @@ class RockInformationController < ApplicationController
   private
 
   def rock_information_params
-    params.require(:rock_information).permit(:name, :region_id, :rock_quality_id, :address, :season1_id, :season2_id, :night_id, :grade_sence_id, :people_day1_id, :people_day2_id, :people_time1_id, :people_time2_id, :people_vibe_id, :other, :image).merge(user_id: current_user.id)
+    params.require(:rock_information).permit(:name, :region_id, :rock_quality_id, :address, :season1_id, :season2_id, :night_id, :grade_sence_id, :people_day1_id, :people_day2_id, :people_time1_id, :people_time2_id, :people_vibe_id, :other, images: []).merge(user_id: current_user.id)
   end
 end
--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,28 +1,50 @@
 if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
-  document.addEventListener('DOMContentLoaded', function(){
-    const ImageList = document.getElementById('image-list');
+  document.addEventListener('DOMContentLoaded', function() {
+     const ImageList = document.getElementById('image-list')
+ 
+     // 選択した画像を表示する関数
+     const createImageHTML = (blob) => {
+        // 画像を表示するためのdiv要素を生成
+       const imageElement = document.createElement('div')
+       imageElement.setAttribute('class', "image-element")
+       let imageElementNum = document.querySelectorAll('.image-element').length
+ 
+       // 表示する画像を生成
+       const blobImage = document.createElement('img')
+       blobImage.setAttribute('src', blob)
 
-    document.getElementById('item-image').addEventListener('change', function(e){
-      // 画像が表示されている場合のみ、すでに存在している画像を削除する
-      const imageContent = document.querySelector('img');
-      if (imageContent){
-        imageContent.remove();
-      }
-      
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
+       //  ファイル選択ボタンを生成
+       const inputHTML = document.createElement('input')
+       inputHTML.setAttribute('id', `rock_information_image_${imageElementNum}`)
+       inputHTML.setAttribute('name', 'rock_information[images][]')
+       inputHTML.setAttribute('type', 'file')
 
-      // 画像を表示するためのdiv要素を生成
-      const imageElement = document.createElement('div');
+       // 生成したHTMLの要素をブラウザに表示させる
+       imageElement.appendChild(blobImage)
+       imageElement.appendChild(inputHTML)
+       ImageList.appendChild(imageElement)
 
-      // 表示する画像を生成
-      const blobImage = document.createElement('img');
-      blobImage.setAttribute('src', blob);
+      //  2枚目以降の画像にもイベント発火するように実装しましょう
+       inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
 
-      // 生成したHTMLの要素をブラウザに表示させる
-      imageElement.appendChild(blobImage);
-      ImageList.appendChild(imageElement);
-    });
-  });
-}
-
+        createImageHTML(blob)
+      })
+     }
+ 
+     document.getElementById('item-image').addEventListener('change', (e) => {
+     // ここから削除(プレビュー画像でつかった　2枚目以降プレビュー画像を削除するという記述)
+     // const imageContent = document.querySelector('img')
+     // if (imageContent){
+     // imageContent.remove();
+     // }
+     // ここまで削除
+ 
+       const file = e.target.files[0];
+       const blob = window.URL.createObjectURL(file);
+ 
+       createImageHTML(blob)
+     });
+   });
+ }
