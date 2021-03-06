21cd822e40db86c10fb97215d436f4c694ff8699
naonao-program <gf19a012@oecu.jp>
2021-03-06 16:46:57 +0900

    ストロングパラメータを記述

--- app/controllers/rock_information_controller.rb
+++ app/controllers/rock_information_controller.rb
@@ -20,6 +20,6 @@ class RockInformationController < ApplicationController
   private
 
   def rock_information_params
-    params.require(:rock_information).permit(:boulder_or_lead_id, :name, :region_id, :rock_quality_id, :address, :season1_id, :season2_id, :night_id, :grade_sence_id, :people_day_id, :people_time1_id, :people_time2_id, :people_vibe_id, :other, images: []).merge(user_id: current_user.id)
+    params.require(:gym_information).permit(:boulder_or_lead_id, :name, :region_id, :rock_quality_id, :address, :season1_id, :season2_id, :night_id, :grade_sence_id, :people_day_id, :people_time1_id, :people_time2_id, :people_vibe_id, :other, images: []).merge(user_id: current_user.id)
   end
 end
