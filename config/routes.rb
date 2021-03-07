498ea89298eca20c952207e087350db235f63da7
naonao-program <gf19a012@oecu.jp>
2021-03-07 12:54:35 +0900

    groupコントローラーにindexアクションを定義しルーティングを設定した

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   resources :rock_information, only:[:index,:new, :create]
   resources :gym_information, only:[:index, :new, :create]
+  resources :group, only:[:index]
 end
