9c8d6cfbf352a21c9d7e9a1d1014722557b41ab8
naonao-program <gf19a012@oecu.jp>
2021-03-10 01:12:40 +0900

    カラムの型間違っていたので変更

--- db/migrate/20210309160718_create_groups.rb
+++ db/migrate/20210309160718_create_groups.rb
@@ -1,7 +1,7 @@
 class CreateGroups < ActiveRecord::Migration[6.0]
   def change
     create_table :groups do |t|
-      t.integer :user_id
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -34,9 +34,10 @@ ActiveRecord::Schema.define(version: 2021_03_09_160718) do
   end
 
   create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "gym_informations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -99,6 +100,7 @@ ActiveRecord::Schema.define(version: 2021_03_09_160718) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "groups", "users"
   add_foreign_key "gym_informations", "users"
   add_foreign_key "rock_informations", "users"
 end
