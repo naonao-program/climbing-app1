ccafd88dbe26140f502f706429f9d4bfd7f6bbd8
naonao-program <gf19a012@oecu.jp>
2021-03-12 02:31:34 +0900

    カード情報のCSSを変更

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -7,5 +7,24 @@ class GroupController < ApplicationController
   end
 
   def create
+    @group = Group.new
+    if @group.valid?
+      pay_item
+      @group.save
+      redirect_to :index
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def pay_item
+    Payjp.api_key = ENV['sk_test_3aed90f3849053e399172023']
+    Payjp::Charge.create(
+      amount: @item[:price],
+      card: order_form_params[:token],
+      currency: 'jpy'
+    )
   end
 end
