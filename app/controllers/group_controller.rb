05a6f760b619998a572d01d5f0b386e4bd8bbd38
naonao-program <gf19a012@oecu.jp>
2021-03-15 15:55:33 +0900

    購入しているときしていないとき表示を切り替えた

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,31 +1,5 @@
 class GroupController < ApplicationController
-  def new
-    @group = Group.new
+  def index
+    @user = current_user.id
   end
-
-  def create
-    @group = Group.new
-    if @group.valid?
-      pay_item
-      @group.save
-      redirect_to :index
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def order_params
-    params.require(:order).permit(:price).merge(token: params[:token])
-  end
-
-  def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    Payjp::Charge.create(
-      amount: order_params[:price],
-      card: order_params[:token],
-      currency:'jpy'
-    )
- end
 end
