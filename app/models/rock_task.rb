a5fadb87467487c551075fca952a514301e65fed
naonao-program <gf19a012@oecu.jp>
2021-03-24 23:38:19 +0900

    バリデーションを記述した

--- app/models/rock_task.rb
+++ app/models/rock_task.rb
@@ -1,2 +1,14 @@
 class RockTask < ApplicationRecord
+  with_options presence: true do
+    validates :images
+    with_options uniqueness: true do
+      validates :name
+    end
+    with_options numericality: { other_than: 1 } do
+      validates :grade_id
+    end
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :grade_id
 end
