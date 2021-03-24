0737f2f9730aff2e4674b2910327cda58edc5e46
naonao-program <gf19a012@oecu.jp>
2021-03-24 22:36:19 +0900

    画像を投稿できるように記述

--- app/controllers/rock_task_controller.rb
+++ app/controllers/rock_task_controller.rb
@@ -7,5 +7,11 @@ class RockTaskController < ApplicationController
   end
 
   def create
+    @task = RockTask.new(rock_task_params)
+    if @task.save
+      redirect_to rock_information_path
+    else
+      render :new
+    end
   end
 end
