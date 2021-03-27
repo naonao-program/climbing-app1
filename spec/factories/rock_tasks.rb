bf1844f3589611de921ca70eee46063bed4d9438
naonao-program <gf19a012@oecu.jp>
2021-03-28 00:37:43 +0900

    モデルの単体テストコードを書いた

--- spec/factories/rock_tasks.rb
+++ spec/factories/rock_tasks.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :rock_task do
-    
+    name                 {'かきくけこ'}
+    rock_task_grade_id   {'2'}
+    other                {'さしすせそ'}
+
+    association :user
+    association :rock_information
+
+    after(:build) do |item|
+      item.images.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
