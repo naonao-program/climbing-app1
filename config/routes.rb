68b76a3f1e9df2be1f8b7d46775bda8ce3c4e20a
naonao-program <gf19a012@oecu.jp>
2021-03-29 00:11:11 +0900

    投稿機能完了

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'top#index'
 
   resources :rock_information do
-    resources :rock_task, only: [:index, :new, :create, :show]
+    resources :rock_task, only: [:new, :create, :show]
     collection do
       get 'search'
     end
