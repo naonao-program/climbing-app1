320c75e163cf4ec889aa2dcf2a567cfdd14f6e48
naonao-program <gf19a012@oecu.jp>
2021-04-05 14:51:47 +0900

    ユーザー新規登録できるときの結合テストコードを書いた

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,9 +1,52 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
+RSpec.describe "ユーザー新規登録", type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.build(:user)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context 'ユーザー新規登録できるとき' do
+    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+      # トップページに移動する
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('Sign Up.')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      fill_in 'Password confirmation', with: @user.password_confirmation
+      fill_in 'Nickname', with: @user.nickname
+      select(value = "2", from: "user[boulder_gym_grade_id]")
+      select(value = "2", from: "user[lead_gym_grade_id]")
+      select(value = "2", from: "user[boulder_rock_grade_id]")
+      select(value = "2", from: "user[lead_rock_grade_id]")
+      select('男性', from: "user[gender_id]")
+      select('北海道', from: "user[region_id]")
+      select(value = "2", from: "user[age_id]")
+      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(1)
+      # トップページへ遷移したことを確認する
+      expect(current_path).to eq(root_path)
+      # ログアウトボタンが表示されることを確認する
+      expect(page).to have_content('Logout.')
+      # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('Sign Up.')
+      expect(page).to have_no_content('Login.')
+    end
+  end
+
+  context 'ユーザー新規登録できないとき' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      # トップページに移動する
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      # 新規登録ページへ移動する
+      # ユーザー情報を入力する
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      # 新規登録ページへ戻されることを確認する
+    end
+  end
 end
