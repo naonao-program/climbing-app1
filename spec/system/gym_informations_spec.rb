e7cef198fbb40c318e2701a9a145eb3928926637
naonao-program <gf19a012@oecu.jp>
2021-04-07 04:27:31 +0900

    編集できるときできないとき　削除できるできないときの結合テストコードを書いた

--- spec/system/gym_informations_spec.rb
+++ spec/system/gym_informations_spec.rb
@@ -46,9 +46,9 @@ RSpec.describe "Gym情報投稿", type: :system do
   context 'Gym情報投稿できないとき' do
     it 'ログインしていない場合は投稿できない' do
       # トップページに遷移する
-      visit root_path
+      visit new_gym_information_path
       # 新規投稿ページへのリンクがない
-      expect(page).to have_no_content('ジム情報投稿')
+      expect(current_path).to eq new_user_session_path
     end
   end
 end
@@ -60,7 +60,7 @@ RSpec.describe "Gym情報編集", type: :system do
     @gym2 = FactoryBot.create(:gym_information)
     @gym_image = Faker::Lorem.sentence
   end
-  context 'ジム情報編集' do
+  context 'ジム情報編集できるとき' do
     it 'ログインしたユーザーは自分が投稿した投稿したものを編集ができる' do
       # ツイート1を投稿したユーザーでログインする
       sign_in(@gym1.user)
@@ -93,6 +93,56 @@ RSpec.describe "Gym情報編集", type: :system do
 
       # トップページに戻ることを確認する
       expect(current_path).to eq gym_information_path(@gym1)
+    end
+  end
+  context 'ジム情報が編集できないとき' do
+    it '投稿者が違う場合は編集できない' do
+      # 投稿した以外のユーザーでログインする
+      sign_in(@gym1.user)
+      # ログイン以外のユーザーの投稿
+      visit gym_information_path(@gym2)
+      # ボタンがないことがある
+      has_no_button?('編集')
+    end
+    it 'ログインしていない場合は編集できない' do
+      visit gym_information_path(@gym2)
+      # ボタンがないことがある
+      has_no_button?('編集')
+    end
+  end
+end
+
+RSpec.describe "Gym情報削除", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+    @gym1 = FactoryBot.create(:gym_information)
+    @gym2 = FactoryBot.create(:gym_information)
+    @gym_image = Faker::Lorem.sentence
+  end
+  context 'ジム情報が削除できるとき' do
+    it 'ログインしているユーザーと投稿しているユーザーが同じ時削除できる' do
+      # ログイン
+      sign_in(@gym1.user)
+      # gym1のユーザーの投稿へ行く
+      visit gym_information_path(@gym1)
+      #ボタンが有ることを確認
+      has_button?('削除')
+      #ボタンを押すとトップページへ行く
+    end
+  end
+
+
+  context 'ジム情報が削除できないとき' do
+    it 'ログインしているユーザーと投稿しているユーザーが同じ時でないとき削除できない' do
+      sign_in(@gym1.user)
+      visit gym_information_path(@gym2)
+      has_no_button?('削除')
+    end
+
+    it 'ログインしていない場合は削除できない' do
+      visit gym_information_path(@gym1)
+      # ボタンがないことがある
+      has_no_button?('編集')
     end
   end
 end
