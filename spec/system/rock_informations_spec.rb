0740d029a8cde185376d182ac7bf9a6db8ab41ec
naonao-program <gf19a012@oecu.jp>
2021-04-28 23:05:27 +0900

    topマルチデバイス対応

--- spec/system/rock_informations_spec.rb
+++ spec/system/rock_informations_spec.rb
@@ -21,11 +21,37 @@ RSpec.describe "Rock情報投稿", type: :system do
       select('ボルダリング',from: "rock_information[boulder_or_lead_id]")
       fill_in 'rock_information_name', with: @rock.name
       select('北海道', from: "rock_information[region_id]")
-      select('石灰岩' ,form: "rock_information[rock_quality_id]")
+      select('石灰岩', from: "rock_information[rock_quality_id]")
       fill_in 'rock_information_address', with: @rock.address
-      select('1月', form: "rock_information[season1_id]")
-      select('1月',form: "rock_information[season2_id]")
-      select('✕',form: "rock_information[night_id]")
+      select('1月', from: "rock_information[season1_id]")
+      select('1月', from: "rock_information[season2_id]")
+      select('✕', from: "rock_information[night_id]")
+      select('全体的に簡単', from: "rock_information[grade_sence_id]")
+      select('平日', from: "rock_information[people_day_id]")
+      select('9時', from: "rock_information[people_time1_id]")
+      select('9時', from: "rock_information[people_time2_id]")
+      select('ワイワイしている', from: "rock_information[people_vibe_id]")
+      fill_in 'rock_information_other', with: @rock.other
+      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { RockInformation.count }.by(1)
+      # トップページへ遷移したことを確認する
+      expect(current_path).to eq(rock_information_index_path)
+    end
+  end
+
+  context 'Rock情報が投稿できないとき' do
+    it '誤った情報では投稿できない' do
+      # トップページに「岩場情報投稿」という文字があるか
+      visit root_path
+      expect(page).to have_content('岩場情報投稿')
+      # ログインする
+      sign_in(@user)
+      # rock投稿ページに行く
+      visit new_rock_information_path
+      #フォームに情報を記入
+
     end
   end
 end
